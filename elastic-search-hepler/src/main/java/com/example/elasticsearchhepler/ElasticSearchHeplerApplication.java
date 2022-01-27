3ea1b45a95f9730d648f3a9010e40d950467d314
fangzy <fangzy0723@sina.com>
2022-01-27 22:17:41 +0800

    添加基础项目

---
+++ elastic-search-hepler/src/main/java/com/example/elasticsearchhepler/ElasticSearchHeplerApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchhepler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticSearchHeplerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticSearchHeplerApplication.class, args);
+    }
+
+}
---
+++ elastic-search-hepler/src/test/java/com/example/elasticsearchhepler/ElasticSearchHeplerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.elasticsearchhepler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticSearchHeplerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("0");
+    }
+
+}
